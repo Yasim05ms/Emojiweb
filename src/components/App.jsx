069288b63ccdb6fb24ceaca769eb5a76e40eba6e
Yasim05ms <97211191+Yasim05ms@users.noreply.github.com>
2023-07-20 08:27:56 +0530

    Add files via upload

---
+++ src/components/App.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import Card from "./Card";
+import emojipedia from "../emojipedia";
+
+function createCard(props) {
+  return (
+    <Card
+      key={props.id}
+      emoji={props.emoji}
+      name={props.name}
+      meaning={props.meaning}
+    />
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <h1>
+        <span>emojipedia</span>
+      </h1>
+
+      <dl className="dictionary">{emojipedia.map(createCard)}</dl>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/emojipedia.js
@@ -0,0 +1,46 @@
+const emojipedia = [
+  {
+    id: 1,
+    emoji: "💪",
+    name: "Tense Biceps",
+    meaning:
+      "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym."
+  },
+  {
+    id: 2,
+    emoji: "🙏",
+    name: "Person With Folded Hands",
+    meaning:
+      "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you."
+  },
+  {
+    id: 3,
+    emoji: "🤣",
+    name: "Rolling On The Floor, Laughing",
+    meaning:
+      "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“."
+  },
+  {
+    id: 4,
+    emoji: "😀",
+    name: "Grinning Face",
+    meaning:
+      "A yellow face with simple, open eyes and a broad, open smile, showing upper teeth and tongue on some platforms. Often conveys general pleasure and good cheer or humor.“."
+  },
+  {
+    id: 5,
+    emoji: "😭",
+    name: "Loudly Crying Face",
+    meaning:
+      "A yellow face with an open mouth wailing and streams of heavy tears flowing from closed eyes. May convey inconsolable grief but also other intense feelings, such as uncontrollable laughter, pride or overwhelming joy“."
+  },
+  {
+    id: 6,
+    emoji: "",
+    name: "Melting Face",
+    meaning:
+      "A yellow smiley face melting into a puddle.  The eyes and mouth slip down the face, yet still maintain a distorted smile. This quality lends this emoji to sarcasm.Can be used literally to talk about extreme heat. Can also be used metaphorically to talk about embarrassment, shame, a slowly sinking sense of dread, or feeling overwhelmed in some way.“."
+  }
+];
+
+export default emojipedia;
